39627342253cb95317a29851e1c4821c58996f4d
DenisAndrew77 <denis77andrew@gmail.com>
2026-06-30 17:16:21 +0300

    defined base paths for first 3 steps of onboarding

--- OnboardingFrontend/src/App.tsx
+++ OnboardingFrontend/src/App.tsx
@@ -1,8 +1,17 @@
-import './App.css'
+import { Navigate, Route, Routes } from 'react-router'
+import { ConfirmProfilePage } from './pages/ConfirmProfilePage'
+import { FindProfilePage } from './pages/FindProfilePage'
 import { WelcomePage } from './pages/WelcomePage'
 
 function App() {
-  return <WelcomePage />
+  return (
+    <Routes>
+      <Route path="/" element={<WelcomePage />} />
+      <Route path="/find-profile" element={<FindProfilePage />} />
+      <Route path="/confirm-profile/:employeeId" element={<ConfirmProfilePage />} />
+      <Route path="*" element={<Navigate replace to="/" />} />
+    </Routes>
+  )
 }
 
 export default App
